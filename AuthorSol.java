0b59e66c956c48b53826bb9d06ec228777fb75b1
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2022-11-20 22:18:35 +0800

    Create AuthorSol.java

---
+++ AuthorSol.java
@@ -0,0 +1,130 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+/*
+
+- the arrangement of characters matters
+- characters with only 1 freq should be removed
+- new length of the string must also be even
+	-> if all freq of characters are even, then the length will also be even
+        
+aaababbb
+aaaabb
+
+focus on forming a pair of identical characters (substring of length 2 i.e., aa, bb, cc, dd, ...)
+
+caacabdabbbdcadcd
+aaaabbdd -> removed 9 characters
+
+ */
+
+public class AuthorSol {
+	
+	public static void main(String[] args) {
+		FastScanner fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int T = 1;
+		T = fs.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			char[] s = fs.next().toCharArray();
+			int n = s.length;
+			boolean[] prev = new boolean[26];
+			Arrays.fill(prev, false);
+			int m = 0;
+			for (int i = 0; i < n; i++) {
+				if (prev[s[i]-'a'] == true) {
+					m += 2;
+					Arrays.fill(prev, false);
+				} else {
+					prev[s[i]-'a'] = true;
+				}
+			}
+			System.out.println(n - m);
+		}
+		out.close();
+	}
+		
+	static final Random rnd = new Random();
+	
+	static void shuffleSort(int[] a) { //change this
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			int newInd = rnd.nextInt(n);
+			int temp = a[newInd]; //change this
+			a[newInd] = a[i];
+			a[i] = temp;
+		}
+		Arrays.sort(a);
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while (!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextInt();
+			}
+			return a;
+		}
+		
+		long[] readLongArray(int n) {
+			long[] a = new long[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextLong();
+			}
+			return a;
+		}
+		
+		double[] readDoubleArray(int n) {
+			double[] a = new double[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextDouble();
+			}
+			return a;
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				if (st.hasMoreTokens()) {
+					str = st.nextToken("\n");
+				} else {
+					str = br.readLine();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
